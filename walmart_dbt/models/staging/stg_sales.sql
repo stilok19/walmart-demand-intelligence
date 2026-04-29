34ee3786a0d04924d4935fc24660d9aa050cff9f
May Tilokruangchai <s.tilokruangchai@gmail.com>
2026-04-28 20:19:21 -0400

    feat: update the name of table create by BigQuery directly as raw to seperate from stg that create from dbt

--- walmart_dbt/models/staging/stg_sales.sql
+++ walmart_dbt/models/staging/stg_sales.sql
@@ -3,4 +3,4 @@
 -- stg_sales pre-built via ingestion/generate_unpivot_sql.py
 -- Using view to avoid duplicating stg_sales_unpivot in BigQuery
 -- create view in order to use {{ ref('stg_sales') }} similar to other staging data that come from dbt
-SELECT * FROM `walmart_forecasting.stg_sales_unpivot`
+SELECT * FROM `walmart_forecasting.raw_sales_train_unpivot`
